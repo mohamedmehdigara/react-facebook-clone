518caaac08832113028dadbeae8fc7bfcfef750c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-05 18:40:18 +0100

    added files

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
+  const handleLogin = () => {
+    // Add login functionality here
+    // You can use an authentication library or implement your own logic
+    console.log("Login button clicked");
+  };
+
   return (
     <div className="login">
       <div className="login__logo">
@@ -9,10 +15,13 @@ function Login() {
       </div>
       <div className="login__container">
         <h1>Welcome to Facebook</h1>
-        <button>Login with Facebook</button>
+        <button className="login__button" onClick={handleLogin}>
+          Login with Facebook
+        </button>
       </div>
     </div>
   );
 }
 
 export default Login;
+
