03199442abd8110345dcf7884e2620dc3eec3491
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-05 18:36:59 +0100

    added files

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,9 +4,24 @@ import './Feed.css';
 function Feed() {
   return (
     <div className="feed">
-      {/* Feed content */}
+      <div className="feed__header">
+        <h2>News Feed</h2>
+      </div>
+      <div className="feed__post">
+        <div className="post">
+          {/* Post content */}
+        </div>
+        <div className="post">
+          {/* Post content */}
+        </div>
+        <div className="post">
+          {/* Post content */}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Feed;
+
+
